741dd6b680e5aee5055b848fe766a558799107e0
santoshsampath <santu.burada99@gmail.com>
2020-08-08 09:37:17 +0530

    Removed animation and completed feedback form

--- app/src/main/java/com/example/starvelater/activities/helpandsupport/Feedback.java
+++ app/src/main/java/com/example/starvelater/activities/helpandsupport/Feedback.java
@@ -3,7 +3,10 @@ package com.example.starvelater.activities.helpandsupport;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,19 +16,37 @@ import com.example.starvelater.activities.user.UserDashboard;
 
 public class Feedback extends AppCompatActivity {
 
-
+    RatingBar simpleRatingBar;
     ImageView btnBack;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feedback_form);
+
+
         btnBack=findViewById(R.id.back_button);
+        simpleRatingBar = findViewById(R.id.rating);
+        submit = findViewById(R.id.submit);
+
+
+
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent back = new Intent(Feedback.this, UserDashboard.class);
-                back.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(back);
+//                Intent back = new Intent(Feedback.this, UserDashboard.class);
+//                back.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(back);
+                onBackPressed();
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Float rating = simpleRatingBar.getRating();
+                Toast.makeText(Feedback.this, ""+rating, Toast.LENGTH_SHORT).show();
             }
         });
 
